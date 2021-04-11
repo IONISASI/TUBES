1a891af90f32bd8298c064aa78fce747856f7cc8
Christopher Jie <73431564+IONISASI@users.noreply.github.com>
2021-04-11 16:51:46 +0700

    Add files via upload

---
+++ exit.py
@@ -0,0 +1,8 @@
+print("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n)")
+P = str(input())
+
+if (P == 'y') :
+    print("Sayonara, matane!!")
+    saveFiles() 
+else: 
+    ("File belum tersimpan")
