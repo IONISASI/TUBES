b23428519ef2c070eb7146f0fb6252b9493f93e9
Christopher Jie <73431564+IONISASI@users.noreply.github.com>
2021-04-11 16:51:24 +0700

    Delete exit.py

--- exit.py
+++
@@ -1,8 +0,0 @@
-print("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n)")
-P = str(input())
-
-if (P == 'y') :
-    print("Sayonara, matane!!")
-    exit()
-else: 
-    ("File belum tersimpan")
